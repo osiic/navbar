ab8f909ab1520ba30a71cddfb48b0de3448ca207
osiic <osiic.offcl@gmail.com>
2024-04-10 22:57:28 -0800

    oh yeah 03

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const section = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
 const container = document.querySelector(".container");
 
+// ganti container ==> container jika pembungkusnya bukan element
 container.addEventListener("scroll", winScroll);
 
 function winScroll() {
   section.forEach((sec) => {
+    // ganti const top = window.scrollY jika pembungkus scroll bukan element
     const top = section[0].getBoundingClientRect().y * -1;
     const offset = sec.offsetTop;
     const height = sec.offsetHeight;
